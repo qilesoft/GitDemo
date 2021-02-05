ef40ec9334fe14fb6374e0bc563a626ef37d06d1
songliu <317971372@qq.com>
2021-02-05 20:13:47 +0800

    新增按钮-1功能

--- app/src/main/java/com/yxcskj/gitdemo/MainActivity.java
+++ app/src/main/java/com/yxcskj/gitdemo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private int number = 0;
     private TextView textNum;
-    private Button button_add;
+    private Button button_add,button_sub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +20,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textNum = findViewById(R.id.textNum);
         textNum.setText(String.valueOf(number));
         button_add = findViewById(R.id.button_add);
+        button_sub = findViewById(R.id.button_sub);
         button_add.setOnClickListener(this);
+        button_sub.setOnClickListener(this);
     }
 
     @Override
@@ -28,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.button_add:
                 textNum.setText(String.valueOf(++number));
+                break;
+            case R.id.button_sub:
+                textNum.setText(String.valueOf(--number));
                 break;
         }
     }
