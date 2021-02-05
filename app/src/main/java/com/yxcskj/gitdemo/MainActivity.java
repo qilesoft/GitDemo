1206fae460e7fbfc60afaa1506e307c18d6482a6
songliu <317971372@qq.com>
2021-02-05 19:46:42 +0800

    实现点击按钮+1功能

--- app/src/main/java/com/yxcskj/gitdemo/MainActivity.java
+++ app/src/main/java/com/yxcskj/gitdemo/MainActivity.java
@@ -3,12 +3,32 @@ package com.yxcskj.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int number = 0;
+    private TextView textNum;
+    private Button button_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textNum = findViewById(R.id.textNum);
+        textNum.setText(String.valueOf(number));
+        button_add = findViewById(R.id.button_add);
+        button_add.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_add:
+                textNum.setText(String.valueOf(++number));
+                break;
+        }
     }
 }
